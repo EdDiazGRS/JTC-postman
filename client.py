7f74c4a57dfa62eb5ab6bcc518a2f5d526b3e520
EdDiazGRS <124111413+EdDiazGRS@users.noreply.github.com>
2024-03-08 15:48:29 -0500

    Delete client.py

--- client.py
+++
@@ -1,9 +0,0 @@
-import grpc
-import book_pb2
-import book_pb2_grpc
-
-channel = grpc.insecure_channel('localhost:50051')
-stub = book_pb2_grpc.GreeterStub(channel)
-
-response = stub.SayHello(book_pb2.HelloRequest(name='World'))
-print("Greeter client received: " + response.message)
