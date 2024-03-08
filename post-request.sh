bb1fa2c760437de0e55e505ead6daac31d6560d2
EdDiazGRS <124111413+EdDiazGRS@users.noreply.github.com>
2024-03-08 15:48:36 -0500

    Delete post-request.sh

--- post-request.sh
+++
@@ -1,6 +0,0 @@
-curl -X POST -H "Content-Type: application/json" -d '{"title": "Hello", "body": "This is a test", "userId": 1}' https://jsonplaceholder.typicode.com/posts
-data='{
-  "title": "x",
-  "body": "y",
-  "userId": 1
-}'
